57bc006d37ece52d001f7bbe6d636777ec3ae828
Hakjun <hiatus9014@gmail.com>
2022-10-04 14:34:57 +0900

    몫과 나머지

--- src/CurrencyCnt.java
+++ src/CurrencyCnt.java
@@ -10,13 +10,15 @@ public class CurrencyCnt {
             int curr10000 = 10000;
             int curr5000 = 5000;
             int curr1000 = 1000;
-            int curr500 = 500;
+
             //25,000원을 5만원으로 나누었을 때 몫, 나머지
             //25,000원을 각 화폐 단위로 나누었을 때 몫과 나머지를 출력하는 코드를 작성
             //각 화폐 단위는 위와 같이 각각 변수로 선언 해주세요.
 
             System.out.printf("5만원권 %d장 나머지:%d%n",refund/curr50000,refund%curr50000);
             System.out.printf("1만원권 %d장 나머지:%d%n",refund/curr10000,refund%curr10000);
+            System.out.printf("5천원권 %d장 나머지:%d%n",refund/curr5000,refund%curr5000);
+            System.out.printf("1천원권 %d장 나머지:%d%n",refund/curr1000,refund%curr1000);
 
         }
     }
